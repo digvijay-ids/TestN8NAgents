7cce4f3d1c4c0c86f4e4f1fe6d15911095cb414e
digvijay-ids <digvijay.singh@iotaanalytics.com>
2026-04-09 14:00:36 +0530

    Pages Config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/TestN8NAgents/',
   server: {
     host: "::",
     port: 8080,
